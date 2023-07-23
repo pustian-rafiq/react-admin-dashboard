283641b1b9569a1394b92f36533770c4cf11372f
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2023-07-23 22:40:10 +0600

    Design left sidebar and footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,12 @@
 import "./footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <span>Admin Dashboard</span>
+      <span>@React Admin Dashborad-2023</span>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
+import { menu } from "../../data/menuData";
 import "./menu.scss";
 
 const Menu = () => {
-  return <div>Menu</div>;
+  // Map data
+  const menuData = menu.map((menu) => {
+    return (
+      <div className="menu-item" key={menu.id}>
+        <span className="title">{menu?.title}</span>
+        {menu?.listItems.map((item) => (
+          <Link className="list-item" to={item.url} key={item.id}>
+            <img src={item?.icon} alt="" />
+            <span className="menu-item-title">{item.title}</span>
+          </Link>
+        ))}
+      </div>
+    );
+  });
+  return <div className="menu">{menuData}</div>;
 };
 
 export default Menu;
