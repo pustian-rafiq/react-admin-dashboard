5892d2f43429ecf539988dca4837696ffdd098b2
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2023-07-27 20:20:06 +0600

    Design top deals component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,22 @@
+import TopDeals from "../../components/home/TopDeals";
 import "./home.scss";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <div className="box box1">
+        <TopDeals />
+      </div>
+      <div className="box box2">Box</div>
+      <div className="box box3">Box</div>
+      <div className="box box4">Box</div>
+      <div className="box box5">Box</div>
+      <div className="box box6">Box</div>
+      <div className="box box7">Box</div>
+      <div className="box box8">Box</div>
+      <div className="box box9">Box</div>
+    </div>
+  );
 };
 
 export default Home;
